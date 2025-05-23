8fa6cfe9af3c404902a157393a2df7d600a7cb75
trippdavis <trippdavis3@gmail.com>
2025-05-23 10:21:55 -0700

    Add recipe create, update, destroy
    Add form that is used to create and update recipes. Add button to recipe
    details page to destroy recipe.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,9 +1,48 @@
 class RecipesController < ApplicationController
+  before_action :set_recipe, only: %i[ show edit update destroy ]
+
   def index
     @recipes = Recipe.all
   end
 
   def show
-    @recipe = Recipe.find(params[:id])
   end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save
+      redirect_to @recipe
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @recipe.update(recipe_params)
+      redirect_to @recipe
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @recipe.destroy
+    redirect_to recipes_path
+  end
+
+  private
+    def set_recipe
+      @recipe = Recipe.find(params[:id])
+    end
+
+    def recipe_params
+      params.expect(recipe: [ :name ])
+    end
 end
